db7dcb4a286b818db38ba396b9f3b2e0b05498cb
Marcelo Vieira <marcelo_v2@estudante.sc.senai.br>
2024-04-17 13:37:38 -0300

    Criação do Hook contendo useFetch e transferência dele para App.jsx

--- projeto-trilhas/src/App.jsx
+++ projeto-trilhas/src/App.jsx
@@ -1,53 +1,37 @@
 import CardTrilha from "./components/CardTrilha"
 import "./App.css"
 import useFetch from "./hooks/useFetch"
+import { useEffect, useState } from "react";
+import Header from "./components/Header";
 
 function App() {
 
-const dados = useFetch("/dados.json")
+  const [dados, isLoading] = useFetch("/dados.json")
+  const [trilhas, setTrilhas] = useState([])
+
+useEffect(() => {
+  if(!!dados && isLoading){
+    setTrilhas(dados.trilhas)
+  }
+},[dados])
 
 
-  const listaTrilhas = [
-    {
-
-      nomeTrilha: 'Trilha da Costa da Lagoa',
-      cidade: 'Florinópolis',
-      estado: 'SC',
-      duracao: 120,
-      trajeto: 4,
-      dificuldade: 'iniciante',
-      tipo: 'caminhada / trekking',
-      nomeUsuario: 'Guilherme André',
-      urlImagem: 'https://images.pexels.com/photos/917510/pexels-photo-917510.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
-
-    },
-
-    {
-
-      nomeTrilha: 'Trilha do Gravatá',
-      cidade: 'Florinópolis',
-      estado: 'SC',
-      duracao: 30,
-      trajeto: 3,
-      dificuldade: 'intermediária',
-      tipo: 'caminhada / trekking',
-      nomeUsuario: 'Paulo Antunes',
-      urlImagem: 'https://images.pexels.com/photos/917510/pexels-photo-917510.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
-
-    }
-  ]
 
 
   return (
-    <div className="container">
-      <h1 className="titulo">Explore Trilhas</h1>
-      {
+    <>
 
-        listaTrilhas.map((trilha, index) => (
-          <CardTrilha dadosTrilha={trilha} key={index} />
-        ))
-      }
-    </div>
+      <Header />
+      <div className="container">
+        <h1 className="titulo">Explore Trilhas</h1>
+
+        {!!trilhas && trilhas.map((trilha, index) => (
+            <CardTrilha dadosTrilha={trilha} key={index} />
+          ))
+        }
+      </div>
+      {/* {!!dados && JSON.stringify(dados)} */}
+    </>
   )
 }
 
--- projeto-trilhas/src/hooks/useFetch.jsx
+++ projeto-trilhas/src/hooks/useFetch.jsx
@@ -1,33 +1,25 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 
 
 function useFetch(url){
 
     const [dados, setDados] = useState(null);
+    const [isLoading, setIsLoading] = useState(false);
 
+    useEffect(() => {
+setIsLoading(true);
 fetch(url)
 .then((response)=>response.json())
-.then((data) => setDados(data ))
-.catch()
+.then((data) => {
+    setDados(data);
+    setIsLoading(false); });
+// .catch((error) => console.log('error'));
 
+},[url]);
 
-return dados;
+return [dados, isLoading];
 }
 
 export default useFetch
 
-// const [data, setData] = useState(null);
-// const [isLoading, setIsLoading] = useState(false);
-
-// useEffect(() => {
-//     setIsLoading(true);
-//     fetch(url)
-//         .then((res) => res.json())
-//         .then((value) => {
-//             setData(value);
-//             setIsLoading(false);
-//         });
-// }, [url]);
-
-// return [data, isLoading];
